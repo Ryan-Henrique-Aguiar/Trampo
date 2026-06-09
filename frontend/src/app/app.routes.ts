d5e221d4259a027ec807bfc789b002649348aa19
marcusRodrigues9 <marcus.rodriguescv@gmail.com>
2026-06-09 17:01:46 -0300

    feat: adiciona rotas

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,39 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { SignIn } from './views/account/sign-in/sign-in';
+import { SignUp } from './views/account/sign-up/sign-up';
+import { Home } from './pages/home/home';
+import { Main } from './pages/main/main';
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+
+  {
+    path: 'login',
+    component: SignIn
+  },
+
+  {
+    path: 'register',
+    component: SignUp
+  },
+
+  {
+    path: 'home',
+    component: Home
+  },
+
+  {
+    path: 'main',
+    component: Main
+  },
+
+  {
+    path: '**',
+    redirectTo: 'login'
+  }
+];
