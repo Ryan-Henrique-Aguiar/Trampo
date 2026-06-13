2881f0289e85d9132e771630bb6dc8155d90a032
marcusRodrigues9 <marcus.rodriguescv@gmail.com>
2026-06-13 11:32:33 -0300

    feat: adiciona service para trazer as cidades/estados usando api do IBGE, ajusta register ui solitando todos dados necessarios para cadastro.

--- frontend/src/app/views/account/sign-up/sign-up.ts
+++ frontend/src/app/views/account/sign-up/sign-up.ts
@@ -1,11 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { AuthLayout } from "../../../shared/components/auth-layout/auth-layout";
 import { RouterLink } from '@angular/router';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LocationService, Cidade, Estado } from '../../../services/location/location';
 
 @Component({
   selector: 'app-sign-up',
-  imports: [AuthLayout, RouterLink],
+  imports: [AuthLayout, RouterLink, FormsModule, ReactiveFormsModule],
   templateUrl: './sign-up.html',
   styleUrl: './sign-up.css',
 })
-export class SignUp {}
+export class SignUp implements OnInit {
+  private locationService = inject(LocationService);
+
+  // Location
+  estadoControl = new FormControl('');
+  cidadeControl = new FormControl({ value: '', disabled: true });
+  estados: Estado[] = [];
+  cidades: Cidade[] = [];
+
+  // Steps
+  isPrestador = false;
+  currentStep = 1;
+
+  ngOnInit() {
+    this.locationService.getEstados().subscribe(data => this.estados = data);
+  }
+
+  onEstadoChange() {
+    const estado = this.estados.find(e => e.nome === this.estadoControl.value);
+    if (!estado) return;
+
+    this.cidades = [];
+    this.cidadeControl.disable();
+
+    this.locationService.getCidades(estado.id).subscribe(data => {
+      this.cidades = data;
+      this.cidadeControl.enable();
+    });
+  }
+
+  nextStep() {
+    if (this.currentStep === 2 && !this.isPrestador) return;
+    this.currentStep++;
+  }
+
+  prevStep() {
+    this.currentStep--;
+  }
+  
+}
