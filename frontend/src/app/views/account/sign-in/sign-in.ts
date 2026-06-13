879546105e959086beba6be45fa4df3ac27aca67
marcusRodrigues9 <marcus.rodriguescv@gmail.com>
2026-06-13 10:26:06 -0300

    feat: ajusta responsvididade no auth-banner

--- frontend/src/app/views/account/sign-in/sign-in.ts
+++ frontend/src/app/views/account/sign-in/sign-in.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { AuthLayout } from "../../../shared/components/auth-layout/auth-layout";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-sign-in',
-  imports: [AuthLayout],
+  imports: [AuthLayout, RouterLink],
   templateUrl: './sign-in.html',
   styleUrl: './sign-in.css',
 })
--- frontend/src/app/views/account/sign-up/sign-up.ts
+++ frontend/src/app/views/account/sign-up/sign-up.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { AuthLayout } from "../../../shared/components/auth-layout/auth-layout";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
-  imports: [AuthLayout],
+  imports: [AuthLayout, RouterLink],
   templateUrl: './sign-up.html',
   styleUrl: './sign-up.css',
 })
