9b35c7e4ea8b57a160f5717b6060c7c2c6bfd294
ryan <ryan.aguiarh@gmail.com>
2026-08-13 17:02:22 -0300

    feat(security): adiciona configuração de segurança da aplicação

---
+++ backend/backend/src/main/java/br/com/trampo/backend/infra/security/SecurityConfigurations.java
@@ -0,0 +1,21 @@
+package br.com.trampo.backend.infra.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfigurations {
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
+}
