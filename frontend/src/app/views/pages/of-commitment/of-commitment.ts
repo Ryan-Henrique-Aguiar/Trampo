44faf4e8be7073fd4087f43aa570811280a05c11
Saimon0605 <siamonrr123@gmail.com>
2026-08-29 09:09:55 -0300

    feat: adiciona botao na tela de termo de compromisso

--- frontend/src/app/views/pages/of-commitment/of-commitment.ts
+++ frontend/src/app/views/pages/of-commitment/of-commitment.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-ofcommitment',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './of-commitment.html',
   styleUrl: './of-commitment.css',
 })
