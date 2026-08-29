44ed2668a90c57c6d33cbc294908c424a1763f61
Saimon0605 <siamonrr123@gmail.com>
2026-08-29 09:27:59 -0300

    feat: adiciona botao de voltar na tela de suporte

--- frontend/src/app/views/pages/support/support.ts
+++ frontend/src/app/views/pages/support/support.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-support',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './support.html',
   styleUrl: './support.css'
 })
@@ -48,3 +49,4 @@ export class Support {
     }, 900);
   }
 }
+  
