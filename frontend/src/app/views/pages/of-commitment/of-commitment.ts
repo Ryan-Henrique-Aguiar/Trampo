6dd8197bea4de6d336782b7f18939708f8570b3f
Saimon0605 <siamonrr123@gmail.com>
2026-08-29 08:23:53 -0300

    feat: criacao da estrutura dos componentes, termo de compromisso, faq, suporte e contato

---
+++ frontend/src/app/views/pages/of-commitment/of-commitment.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-of-commitment',
+  imports: [],
+  templateUrl: './of-commitment.html',
+  styleUrl: './of-commitment.css',
+})
+export class OfCommitment {}
